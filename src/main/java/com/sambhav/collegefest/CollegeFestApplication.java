26ab6cc8917d8a40188cbb352cfe8916dbacdf7e
mr-possible <sambhavd4@gmail.com>
2021-09-11 22:07:54 +0530

    Initial commit.

---
+++ src/main/java/com/sambhav/collegefest/CollegeFestApplication.java
@@ -0,0 +1,11 @@
+package com.sambhav.collegefest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegeFestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CollegeFestApplication.class, args);
+    }
+}
