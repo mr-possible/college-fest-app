874134518b7e4c1bbcc7732c2a3c4eb76db4d03c
mr-possible <sambhavd4@gmail.com>
2021-09-18 21:31:20 +0530

    Updated sql file.

--- college-fest-springboot/sql/eventstable.sql
+++ college-fest-springboot/sql/eventstable.sql
@@ -11,20 +11,21 @@ CREATE TABLE eventstable (
                              eventtime TIME,
                              eventvenue VARCHAR(20),
                              eventorganiser VARCHAR(20),
-                             eventorganisercontact VARCHAR(10)
+                             eventorganisercontact VARCHAR(10),
+                             eventfee NUMERIC(5)
 );
 
-INSERT INTO eventstable (eventid, eventcategory,eventname,eventdesc,eventdate,eventtime,eventvenue,eventorganiser,eventorganisercontact)
-VALUES ('fri1600', 'Gaming', 'game with me', 'A gaming Competition', '2020-11-13', '12:00', 'Clubhouse','Darshil Dutta', '9595995852');
+INSERT INTO eventstable (eventid, eventcategory,eventname,eventdesc,eventdate,eventtime,eventvenue,eventorganiser,eventorganisercontact,eventfee)
+VALUES ('fri1600', 'Gaming', 'game with me', 'A gaming Competition', '2020-11-13', '12:00', 'Clubhouse','Darshil Dutta', '9595995852',200);
 
-INSERT INTO eventstable (eventid, eventcategory, eventname, eventdesc, eventdate, eventtime, eventvenue, eventorganiser, eventorganisercontact)
-VALUES ('mon1400', 'Poetry writing and recitation','Write a poem', 'A poetry Competition', '2020-11-14', '03:00','Clubhouse', 'Rimjhim Dutta', '9595995852');
+INSERT INTO eventstable (eventid, eventcategory, eventname, eventdesc, eventdate, eventtime, eventvenue, eventorganiser, eventorganisercontact,eventfee)
+VALUES ('mon1400', 'Poetry writing and recitation','Write a poem', 'A poetry Competition', '2020-11-14', '03:00','Clubhouse', 'Rimjhim Dutta', '9595995852',140);
 
-INSERT INTO eventstable (eventid, eventcategory,eventname,eventdesc,eventdate, eventtime,eventvenue,eventorganiser,eventorganisercontact)
-VALUES ('tue1500', 'Story Writing','Write a story', 'A story writing Competition', '2020-11-15', '06:00','Clubhouse','Sanjeev Dutta', '9595995852');
+INSERT INTO eventstable (eventid, eventcategory,eventname,eventdesc,eventdate, eventtime,eventvenue,eventorganiser,eventorganisercontact,eventfee)
+VALUES ('tue1500', 'Story Writing','Write a story', 'A story writing Competition', '2020-11-15', '06:00','Clubhouse','Sanjeev Dutta', '9595995852',200);
 
-INSERT INTO eventstable (eventid, eventcategory, eventname, eventdesc, eventdate, eventtime, eventvenue, eventorganiser, eventorganisercontact)
-VALUES ('fri1400', 'Short Film Making','Filmmaking', 'A shortfilm making Competition', '2020-11-16', '08:00','Clubhouse','Piyali Dutta', '9595995852');
+INSERT INTO eventstable (eventid, eventcategory, eventname, eventdesc, eventdate, eventtime, eventvenue, eventorganiser, eventorganisercontact,eventfee)
+VALUES ('fri1400', 'Short Film Making','Filmmaking', 'A shortfilm making Competition', '2020-11-16', '08:00','Clubhouse','Piyali Dutta', '9595995852',400);
 
 SELECT
     *
